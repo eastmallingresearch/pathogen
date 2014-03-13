2feee3de0b505c497db3fa560fd059ceb01bb2a8
Richard Harrison <richard.harrison@emr.ac.uk>
2014-03-13 15:43:34 +0000

    Added more arguments and made SGE compatible

--- align.sh
+++ align.sh
@@ -1,11 +1,20 @@
 #!/bin/bash 
 #Assemble contigs using velvet and generate summary statistics using process_contigs.pl
-WORK_DIR=P.cact_assembly
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 4
+#$ -l virtual_free=4G
+
+WORK_DIR=$TMPDIR
 HASH_LENGTH=$1
-FORWARD_FILE=P.cact_411_1M_F_trim.fastq
-REVERSE_FILE=P.cact_411_1M_R_trim.fastq
-ASSEMBLY_NAME=P.cact
+FORWARD_FILE=$2
+REVERSE_FILE=$3
+ASSEMBLY_NAME=$4
+
 echo "HASH LENGTH IS  $HASH_LENGTH"
+echo "FORWARD FILE IS $FORWARD_FILE"
+echo "Reverse file is $REVERSE_FILE"
+echo "Assembly name is $ASSEMBLY_NAME"
 
 velveth $WORK_DIR $HASH_LENGTH -fastq -shortPaired -separate $FORWARD_FILE $REVERSE_FILE
 velvetg $WORK_DIR -long_mult_cutoff 1 -exp_cov 6 -ins_length 700 -cov_cutoff 2 -min_contig_lgth 750
