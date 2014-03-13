c422b8f473c9c804a44db9e54b3ea8e828665dac
Richard Harrison <richard.harrison@emr.ac.uk>
2014-03-13 22:59:36 +0000

    Additional notes

--- rxlr/rxlr_pipeline.sh
+++ rxlr/rxlr_pipeline.sh
@@ -9,16 +9,15 @@ echo "REVCOMPing the contigs"
 echo "Predicting coding seqs- reverse"
 #~/scripts/rxlr/print_atg_50FaN2.pl contigs_R.fa >atg_R.fa
 #cat atg.fa atg_R.fa>aa_cat.fa
-echo "Signal P- this can be parallelised"
-#~/scripts/rxlr/run_signalP3.pl ~/signalp-2.0/ aa_cat.fa
-~/scripts/rxlr/parallel_signalp.sh 
+echo "Signal P- this is now  parallelised"
+#~/scripts/rxlr/parallel_signalp.sh 
 rm -f submit_*
 cd fasta_seqs
 cat *.faa.out >all.out
 cd ..
 mv ./fasta_seqs/all.out .
-echo "Annotating signal p data "
-~/scripts/rxlr/annotate_signalP2hmm3_v2.pl all.out all.sp.tab all.sp.pve all.sp.nve all.sp.faa
+echo "Annotating signal p data - this script needs work"
+~/scripts/rxlr/annotate_signalP2hmm3_v2.pl all.out all.sp.tab all.sp.pve all.sp.nve aa_cat.fa
 
 
 
