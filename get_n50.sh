6080b8907e0302e1af0f3f32741b4c00c0264492
Richard Harrison <richard.harrison@emr.ac.uk>
2014-03-13 16:09:58 +0000

    Simple script to aggregate N50 values

---
+++ get_n50.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTPUT_FILE=n50.dat
+echo "hash_length N50"> $OUTPUT_FILE
+
+for DIR in $( ls -d P.cact.auto.* ); do
+	HASH_LENGTH=$( echo $DIR | cut -f4 -d'.' )
+	STATS_FILE=$DIR/stats.txt
+	N50=$( head -n1 $STATS_FILE | cut -f2 -d' ')
+	# echo "dir = $DIR and hash length = $HASH_LENGTH"
+	echo "$HASH_LENGTH $N50" >> $OUTPUT_FILE
+
+done 
