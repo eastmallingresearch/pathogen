0db6e93f86b10af59dd26dc3fb6943bc4b868ac7
Andrew Armitage <armita@richards-mbp.emr.ac.uk>
2014-04-16 15:06:05 +0100

    removed qsub commands from beginning of file

--- rxlr/submit_signalp.sh
+++ rxlr/submit_signalp.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#Assemble contigs using velvet and generate summary statistics using process_contigs.pl
-#$ -S /bin/bash
-#$ -cwd
-#$ -pe smp 16
-#$ -l virtual_free=1G
 
 FILE_NAME=$1
 PATH_TO_SP=$2
