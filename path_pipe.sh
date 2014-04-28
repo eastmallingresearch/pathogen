48f28baa66f398f6fc87660175d0f54f872b3016
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-04-28 17:23:36 +0100

    path_pipe.sh is being developed as a stand alone shell script that does not call on any other shell scripts. It still calls on other .pl tools.

--- path_pipe.sh
+++ path_pipe.sh
@@ -16,6 +16,8 @@ CUR_PATH=$PWD
 WORK_DIR=/tmp/path_pipe
 
 IN_FILE=$1
+
+SCRIPT_DIR=/home/armita/git_repos/pathogen/rxlr
 SIG_P=/home/master_files/prog_master/bin
 
 ORGANISM=$(echo $IN_FILE | cut -d "/" -f3)
@@ -26,6 +28,14 @@ mkdir $WORK_DIR
 cd $WORK_DIR
 cp $CUR_PATH/$IN_FILE .
 
+
+
+
+#########################################################################
+
+#########################################################################
+
+
 #######  Step 1	 ########
 # Run RXLR part1		#
 #########################
@@ -34,25 +44,162 @@ cp $CUR_PATH/$IN_FILE .
 # signalp2 as your second argument"
 
 
-/home/armita/git_repos/pathogen/rxlr/rxlr_pipeline_part1.sh $SORTED_CONTIGS $SIG_P
+# /home/armita/git_repos/pathogen/rxlr/rxlr_pipeline_part1.sh $SORTED_CONTIGS $SIG_P
 
 
-#######  Step 1	 ########
+INPUT=$SORTED_CONTIGS
+
+echo $SCRIPT_DIR
+
+
+	#######  Step 1a ########
+	# find open reading 	#
+	# frames in contigs and #
+	# translate these to AA	#
+	#########################
+
+echo "RxLR pipeline- input your sorted contigs as your first argument and the path to signalp2 as your second argument"
+echo "Predicting coding seqs- forward"
+$SCRIPT_DIR/print_atg_50FaN2.pl $INPUT F >atg.fa
+
+	#######  Step 1b ########
+	# revcomp contigs to get#
+	# reads on the R strand #
+	# of DNA				#
+	#########################
+
+
+echo "REVCOMPing the contigs"
+$SCRIPT_DIR/revcomp_fasta.pl $INPUT >contigs_R.fa
+
+ 
+	#######  Step 1c ########
+	# find open reading 	#
+	# frames in revcomp		#
+	# contigs and translate	# 
+	# these to AA			#
+	#########################
+	
+	
+echo "Predicting coding seqs- reverse"
+$SCRIPT_DIR/print_atg_50FaN2.pl contigs_R.fa R >atg_R.fa
+
+ 
+	#######  Step 1d ########
+	# concatenate files of	#
+	# open reading frames	#
+	#########################
+
+echo "Joining Forward and Reverse Files"
+cat atg.fa atg_R.fa>aa_cat.fa
+
+	#######  Step 1e ########
+	# 		Cleanup			#
+	#########################
+
+echo "Cleaning up any old files"
+rm -rf ./fasta_seq
+
+	#######  Step 1f ########
+	# Split ORF files into  #
+	# batches of 500 		#
+	# 		proteins		#
+	#########################
+	
+echo "Outputting batch FASTA files for Signal P"
+$SCRIPT_DIR/run_signalP3.pl aa_cat.fa
+
+	#######  Step 1g ########
+	# Run Signal P on 		#
+	# batched protein files	#
+	#########################
+
+echo "Signal P- this is now  parallelised"
+$SCRIPT_DIR/parallel_signalp.sh $SIG_P
+
+
+
+
+#########################################################################
+
+#########################################################################
+
+
+#######  Step 2	 ########
 # Run RXLR part2		#
 #########################
 
-/home/armita/git_repos/pathogen/rxlr/rxlr_pipeline_part2.sh
+#	/home/armita/git_repos/pathogen/rxlr/rxlr_pipeline_part2.sh
 
-#######  Step 1	 ########
+
+SCRIPT_DIR=/home/armita/git_repos/pathogen/rxlr
+
+	#######  Step 2a ########
+	# 	Remove SGE files	#
+	#########################
+
+
+echo "Tidying SGE files"
+rm -f submit_*
+
+	#######  Step 2b ########
+	# 	Concatenate batch	#
+	# SGE files into single #
+	# outfile				#
+	#########################
+
+echo "Concatonating files"
+cat ./fasta_seqs/*.faa.out >all.out
+
+
+	#######  Step 2c ########
+	# 	Annotate AA file with
+	#	signalP	output data #
+	#########################
+	
+	
+echo "Annotating signal p data "
+tail -n +5 all.out>all.f.out
+$SCRIPT_DIR/annotate_signalP2hmm3_v3.pl all.f.out all.sp.tab all.sp.pve all.sp.nve aa_cat.fa
+
+	#######  Step 2d ########
+	# 	motif search for	#
+	# rxlrs in aa sequence	#
+	#########################
+
+echo "RxLR prediction" 
+$SCRIPT_DIR/find_rxlr_v2.pl all.sp.pve all.sp.rxlr
+
+
+
+
+#########################################################################
+
+#########################################################################
+
+#######  Step 3	 ########
 # Run MIMP detection	#
 #########################
 
+	#######  Step 3a ########
+	# 	motif search for
+	# MIMPs in aa sequence	#
+	#########################
+
 /home/armita/git_repos/pathogen/mimp_finder.pl $SORTED_CONTIGS $STRAIN_mimps.fa
 
-#######  Step 1	 ########
+
+
+#########################################################################
+
+#########################################################################
+
+#######  Step 4	 ########
 # 		Cleanup			#
 #########################
 
+
+mkdir $CUR_PATH/analysis/rxlr/$ORGANISM
 
 mkdir $CUR_PATH/analysis/rxlr/$ORGANISM/$STRAIN/
 
