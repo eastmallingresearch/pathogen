8adf9c685f5286b2ba6912457e6c9f6a55cdb25d
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-07-30 11:33:55 +0100

    1) Now outputs co-ordinates of hit. 2) No longer multiplies length of the query and hit by 3 to give length in bp from aa sequence 3) Removed hashed commands.

--- blast/blast2csv.pl
+++ blast/blast2csv.pl
@@ -12,7 +12,6 @@ use Bio::Search::Result::BlastResult;
 my $usage = "blast2csv.pl <query_file.fa> <database_name> > <outfile.csv>";
 my $query_file = shift or die $usage;
 my $database = shift or die $usage;
-#my $blast_obj;
 my $blast_fac;
 my $result_obj;
 my $report_obj;
@@ -23,14 +22,13 @@ my $seq_obj;
 # 		Step 1.		Set outfile header
 #-------------------------------------------------------
 
-my $outline_header = "ID\tSequence\tSequence_lgth\tNo.hits\tBest_hit\tE-value\tHit_lgth\tPer_length\tPer_ID\tHit_seq\n";
+my $outline_header = "ID\tSequence\tSequence_lgth\tNo.hits\tBest_hit\tE-value\tHit_lgth\tPer_length\tPer_ID\tHit_strand\tHit_start\tHit_end\tHit_seq\n";
 print "$outline_header";
 
 #-------------------------------------------------------
 # 		Step 2.		Create BLAST factory
 #-------------------------------------------------------
  
-#$blast_obj = Bio::Tools::Run::StandAloneBlast->new('-program'  => 'tblastn', '-database' => $database);
 $blast_fac = Bio::Tools::Run::StandAloneBlastPlus->new(
 												'-db_name' => 'genome_db', 
 												'-db_dir' => '.', 
@@ -54,13 +52,8 @@ my $input_obj = Bio::SeqIO->new('-file' => $query_file, '-format' => 'fasta', '-
 #-------------------------------------------------------
  
 while (my $seq = $input_obj->next_seq) {
-#	my $seq = $seq_obj->seq;
-# 	$report_obj = $blast_obj->blastall($seq);
-# 	$result_obj = $report_obj->next_result;
+
 	$report_obj = $blast_fac->run('-method' => 'tblastn', '-query' => $seq );
-#	$result_obj = $report_obj->next_result;
-#	$result_obj = $report_obj->next_hit;
-# 	my @ao_hits = $result_obj->hits;
 	my @ao_hits = $report_obj->hits;
   	my $hit = $ao_hits[0];
 
@@ -69,8 +62,7 @@ while (my $seq = $input_obj->next_seq) {
 #-------------------------------------------------------	
  	my $seq_id =  $seq->id;
 	my $sequence = $seq->seq;
-	my $query_lgth = (($seq->length)*3);
-#	my $no_hits = $result_obj->num_hits;
+	my $query_lgth = ($seq->length);
 	my $no_hits = $report_obj->num_hits;
 
 #-------------------------------------------------------
@@ -85,21 +77,27 @@ while (my $seq = $input_obj->next_seq) {
 	my $e_value;		
  	my $hit_seq;
  	my $outline;
+ 	my $strand;
+ 	my $start;
+ 	my $end;
  	
  	if ($hit) {  					
  		my $hsp = $hit->hsp('best');
- 		my $hit_id = substr $hit->name(), 4;
-		my $hit_seq = $hsp->seq_str; 	
- 		my $hit_lgth = length ($hit_seq);
- 		my $per_query = ($hit_lgth / $query_lgth);
+ 		$hit_id = substr $hit->name(), 4;
+		$hit_seq = $hsp->seq_str; 	
+ 		$hit_lgth = length ($hit_seq);
+ 		$per_query = ($hit_lgth / $query_lgth);
  		$per_query = substr $per_query, 0, 4;
- 		my $ident = $hsp->num_identical();
-		my $per_id = $hit->frac_identical;
+ 		$ident = $hsp->num_identical();
+		$per_id = $hit->frac_identical;
 		$per_id = substr $per_id, 0, 4;
-		my $e_value = $hsp->evalue;			
+		$e_value = $hsp->evalue;
+		$strand = $hsp->strand('hit');
+		$start = $hsp->start('hit');
+		$end = $hsp->end('hit');			
 
  		
- 		$outline = "$seq_id\t$sequence\t$query_lgth\t$no_hits\t$hit_id\t$e_value\t$hit_lgth\t$per_query\t$per_id\t$hit_seq\n";	
+ 		$outline = "$seq_id\t$sequence\t$query_lgth\t$no_hits\t$hit_id\t$e_value\t$hit_lgth\t$per_query\t$per_id\t$strand\t$start\t$end\t$hit_seq\n";	
  	}
  	else {
 		$outline = "$seq_id\t$sequence\t$query_lgth\t$no_hits\n";	
