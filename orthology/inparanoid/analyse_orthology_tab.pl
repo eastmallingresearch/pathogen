fe2b1431d4bd7c55edd0d2dad8e154aa87bb9e90
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-11-24 18:11:09 +0000

    1) Now accepts inputs from files containing lists of genes 2) Can be used to perform searches for the orthologs of genes listed as orthologs to the chosen gene

--- orthology/inparanoid/analyse_orthology_tab.pl
+++ orthology/inparanoid/analyse_orthology_tab.pl
@@ -3,14 +3,35 @@
 use strict;
 use warnings;
 
-my $useage = "analyse_orthology_tab.pl <orthology_table.csv>";
+my $usage = "analyse_orthology_tab.pl <orthology_table.csv> [optional: -file list_of_genes.txt -deep]/n";
 
 
-my $infile = shift;
-build_hash ($infile);
-my %orthology_hash = build_hash ($infile);
+my $in_tab = shift or die $usage;
+my $manual_input = 'Yes';
+my $deep_search = 'No';
+my $gene_file;
+foreach (@ARGV) {
+	my $cur_element = shift @ARGV;
+	if ($cur_element eq '-file') {
+		$manual_input = 'No'; 
+		$gene_file = shift @ARGV;
+	} elsif ($cur_element eq '-deep') {
+		$deep_search = 'Yes';
+	}
+}
+	
+build_hash ($in_tab);
+my %orthology_hash = build_hash ($in_tab);
 
-search_gene (%orthology_hash);
+if ($manual_input eq 'Yes') {
+	my $search_again = 'y';
+	while ($search_again eq 'y') {
+		$search_again = manual_search ($deep_search, %orthology_hash);
+	}
+} elsif ($manual_input eq 'No') {
+	from_file ($gene_file, $deep_search, %orthology_hash);
+}
+	
 
 # for my $key ( keys %orthology_hash ) {
 #    print "$key\t", join("\t", @{$orthology_hash{$key}}), "\n";
@@ -28,66 +49,152 @@ exit;
 #
 
 sub build_hash {
-	my ($infilebadger) = @_;
+	my ($infile) = @_;
 	open (INFILE, $infile); 
-	my %hash;
+	my %orthology_hash;
 	while (<INFILE>) {
  		my $cur_line = $_;
  		chomp $cur_line;
   		my @cur_line = split ("\t", $cur_line);
   		my $line_key = shift @cur_line;
  		foreach (@cur_line) {
-			push @{ $hash{$line_key} }, $_;
+			push @{ $orthology_hash{$line_key} }, $_;
  		}
  	}
- 	return (%hash);
+ 	return (%orthology_hash);
  }
 
 #-----------------------------------------------------
 #	Querying for a gene
-sub search_gene {
-	my (%hash) = @_;
-	my $print_out = '';
-	print "Please enter the name of the gene you wish to search for\n";
+sub manual_search {
+	my ($deep_search) = shift @_;
+	my (%orthology_hash) = @_;
+		print "Please enter the name of the gene you wish to search for\n";
 	my $search_name = <STDIN>;
 	chomp $search_name;
 	print "Would you like to print search results to an outfile?\ty\tn\n";
 	my $print_switch = <STDIN>;
 	chomp $print_switch;
-	unless ($print_switch eq 'y'|'n') {print "Error: Answer y or n. Not printing results for now.\n";}
+	unless ($print_switch eq 'y'||'n') {print "Error: Answer y or n. Not printing results for now.\n";}
 	print "\nLooking for gene:\t";
 	print "$search_name\n\n";
-	my @ortholog_groups = @{$orthology_hash{$search_name}} or print "\nError: Can not find gene in hash\n";
+	my %out_hash = ortholog_search ($search_name, $deep_search, %orthology_hash);
 	print "Orthology groups are:\t";
-	print "@ortholog_groups\n\n";
+	print join("\t",$search_name, @{$out_hash{$search_name}}), "\n\n";
 	print "Members of this ortholog group are:\n";
-	for my $key ( keys %hash ) {
-		my @cur_line = @{$hash{$key}};
+	for my $key ( keys %out_hash ) {
+    	print join("\t",$key, @{$out_hash{$key}}), "\n";
+	}	
+	if ($print_switch eq 'y') { sub_print ($search_name, %out_hash)}
+	print "would you like to search again?\ty\tn\n";
+	my $search_again = <STDIN>;
+	chomp $search_again;
+	return $search_again;
+}
+
+
+#-----------------------------------------------------
+# collect a list of genes to search for from a file.
+sub from_file {
+	my ($gene_file) = shift @_;
+	my ($deep_search) = shift @_;
+	my (%orthology_hash) = @_;
+	open INGENES, $gene_file;
+		while (<INGENES>) {
+			my $search_name = $_;
+			chomp $search_name;
+			my %out_hash = ortholog_search ($search_name, $deep_search, %orthology_hash);
+			sub_print ($search_name, %out_hash)
+		}
+}
+
+
+	
+#-----------------------------------------------------
+# search for orthologous genes to the query
+
+sub ortholog_search {
+	my ($search_name) = shift @_;
+	my ($deep_search) = shift @_;
+	my (%orthology_hash) = @_;
+	my %out_hash;
+	my @ortholog_groups = @{$orthology_hash{$search_name}} or print "\nError: Can not find $search_name in hash\n";
+	for my $key ( keys %orthology_hash ) {
+		my @cur_line = @{$orthology_hash{$key}};
 		my @remaining_elements = @cur_line;
 		foreach (@ortholog_groups) {
 			my $this_ortholog = $_;
-			if ($this_ortholog ne '-' && $this_ortholog eq (shift @remaining_elements)) {
-				$print_out .= join ("\t", $key, @cur_line) . "\n";
+			if ($this_ortholog ne '-' && $this_ortholog eq shift @remaining_elements) {
+				if (exists ($out_hash{$key})) {
+				} else {
+					push @{ $out_hash{$key} }, @cur_line;
+				}
 				last;
 			} 
 		}
 	}
-	print "$print_out";
-	if ($print_switch eq 'y') { sub_print ($search_name, $print_out)}
-
+	if ($deep_search eq 'Yes') {
+	#This does a second round of searching to identify all the orthologs listed to the identified orthologs.
+		for my $search_name ( keys %out_hash ) {
+			my @ortholog_groups = @{$orthology_hash{$search_name}} or print "\nError: Can not find $search_name in hash\n";
+			for my $key ( keys %orthology_hash ) {
+				my @cur_line = @{$orthology_hash{$key}};
+				my @remaining_elements = @cur_line;
+				foreach (@ortholog_groups) {
+					my $this_ortholog = $_;
+					if ($this_ortholog ne '-' && $this_ortholog eq shift @remaining_elements) {
+						if (exists ($out_hash{$key})) {
+						} else {
+							push @{ $out_hash{$key} }, @cur_line;
+						}
+						last;
+					} 
+				}
+			}
+		}
+	}
+	return %out_hash;
 }
-	
+
+#-----------------------------------------------------
+# search for orthologs of the orthologs
+
+sub deep_search {
+	my ($search_name) = shift @_;
+	my (%orthology_hash) = shift @_;
+	my (%out_hash) = shift @_;
+	my @ortholog_groups = @{$orthology_hash{$search_name}} or print "\nError: Can not find $search_name in hash\n";
+	for my $key ( keys %orthology_hash ) {
+		my @cur_line = @{$orthology_hash{$key}};
+		my @remaining_elements = @cur_line;
+		foreach (@ortholog_groups) {
+			my $this_ortholog = $_;
+			if ($this_ortholog ne '-' && $this_ortholog eq shift @remaining_elements) {
+				if (exists ($out_hash{$key})) {
+				} else {
+					push @{ $out_hash{$key} }, @cur_line;
+				}
+				last;
+			} 
+		}
+	}
+	return %out_hash;
+}
+
 
 #-----------------------------------------------------
 #	print output
 	
 sub sub_print {
-		my ($search_name, $print_out) = @_;
+		my ($search_name) = shift @_;
+		my (%out_hash) = @_;
 		my $outfile = "$search_name" . ".txt";
 		$outfile =~ tr /\|/_/;
 		open (OUTFILE, ">$outfile") or print "Error: cCould not open outfile.";
-		print OUTFILE "$print_out";
+		for my $key ( keys %out_hash ) {
+    		print OUTFILE join("\t",$key, @{$out_hash{$key}}), "\n";
+		}
+#		print OUTFILE "$print_out";
 		close OUTFILE;
 }
-
 
