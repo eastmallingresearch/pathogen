fb07107a47c88533a4f9ddda4d8362f3b2ad3b5e
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-03-20 18:38:55 +0000

    Now outputs unaligned reads. Also outputs to a more rational directory structure.

--- lineage_specific_regions/bowtie2_alignment_pipe.sh
+++ lineage_specific_regions/bowtie2_alignment_pipe.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #$ -S /bin/bash
 #$ -cwd
-#$ -pe smp 8
+#$ -pe smp 3
 #$ -l virtual_free=1G
 
 
@@ -30,18 +30,18 @@ WORK_DIR=$TMPDIR/bowtie2_"$SAM_FILE"
 mkdir -p $WORK_DIR
 cd $WORK_DIR
 
-fastq-mcf /home/armita/git_repos/emr_repos/tools/seq_tools/illumina_full_adapters.fa $CUR_PATH/$F_READS_IN $CUR_PATH/$R_READS_IN -o $F_FILE -o $R_FILE -C 1000000 -u -k 20 -t 0.01 -q 30
-#cat $CUR_PATH/$F_READS_IN | gunzip -c -f > $F_FILE
-#cat $CUR_PATH/$F_READS_IN | gunzip -c -f > $R_FILE
+#fastq-mcf /home/armita/git_repos/emr_repos/tools/seq_tools/illumina_full_adapters.fa $CUR_PATH/$F_READS_IN $CUR_PATH/$R_READS_IN -o $F_FILE -o $R_FILE -C 1000000 -u -k 20 -t 0.01 -q 30
+cat $CUR_PATH/$F_READS_IN | gunzip -c -f > $F_FILE
+cat $CUR_PATH/$F_READS_IN | gunzip -c -f > $R_FILE
 cp $CUR_PATH/$GENOME_IN $GENOME_FILE 
 
 bowtie2-build $GENOME_FILE $GENOME_INDEX
-bowtie2 -x $GENOME_INDEX -1 $F_FILE -2 $R_FILE -S "$SAM_FILE".sam -p 8
+bowtie2 -x $GENOME_INDEX -1 $F_FILE -2 $R_FILE -S "$SAM_FILE".sam  -p 3 --un-conc "$SAM_FILE"_unaligned.fastq
 samtools view -bS "$SAM_FILE".sam > "$SAM_FILE".bam
 samtools sort "$SAM_FILE".bam "$SAM_FILE"_sorted
 samtools index "$SAM_FILE"_sorted.bam
 samtools faidx $GENOME_FILE
-samtools tview "$SAM_FILE".bam $GENOME_FILE
+#samtools tview "$SAM_FILE"_sorted.bam $GENOME_FILE
 samtools idxstats "$SAM_FILE"_sorted.bam > "$SAM_FILE"_sorted_indexstats.csv
 /home/armita/git_repos/emr_repos/scripts/alternaria/assembly/divide_col.py "$SAM_FILE"_sorted_indexstats.csv 1 2 > "$SAM_FILE"_sorted_indexstats_coverage.csv
 printf "occurence\taligned_reads_per_base\n" > "$SAM_FILE"_reads_per_base.csv
@@ -50,7 +50,7 @@ cat "$SAM_FILE"_sorted_indexstats_coverage.csv | cut -f2,5 | grep -w '0' | cut -
 cat "$SAM_FILE"_sorted_indexstats_coverage.csv | cut -f1,5 | grep -w '0' | grep -v '*' | cut -f1 > "$STRAIN2"_ls_contigs.txt
 /home/armita/git_repos/emr_repos/tools/pathogen/lineage_specific_regions/find_novel_reads.py "$STRAIN2"_ls_contigs.txt $GENOME_FILE -fa > "$STRAIN2"_ls_contigs.fa
 
-mkdir -p $CUR_PATH/assembly/ls_contigs/$ORGANISM2/$STRAIN2
-cp "$SAM_FILE"* $CUR_PATH/assembly/ls_contigs/$ORGANISM2/$STRAIN2/.
-cp "$STRAIN2"_ls_contigs.fa $CUR_PATH/assembly/ls_contigs/$ORGANISM2/$STRAIN2/.
+mkdir -p $CUR_PATH/assembly/ls_contigs/$ORGANISM1/$STRAIN1/vs_"$STRAIN2"_contigs
+cp "$SAM_FILE"* $CUR_PATH/assembly/ls_contigs/$ORGANISM1/$STRAIN1/vs_"$STRAIN2"_contigs/.
+cp "$STRAIN2"_ls_contigs.fa $CUR_PATH/assembly/ls_contigs/$ORGANISM1/$STRAIN1/vs_"$STRAIN2"_contigs/.
 rm -r $WORK_DIR/
