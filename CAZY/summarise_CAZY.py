bf39392c4953b734e7901e4d59662a275425891a
adarmitage <Andrew.Armitage@EMR.ac.uk>
2017-02-27 19:04:58 +0000

    Now allows for a summary of all cazy genes in the entire genome, summary by gene models rather than proteins and a conversion of CAZY annotations to functino identified in Kubicek et al 2014

--- CAZY/summarise_CAZY.py
+++ CAZY/summarise_CAZY.py
@@ -25,6 +25,10 @@ ap.add_argument('--cazy',required=True,type=str,help='input CAZY file')
 ap.add_argument('--inp_secreted',required=True,type=str,help='List of secreted genes')
 ap.add_argument('--inp_gff',required=True,type=str,help='gene annotations to provide a breakdown by contig')
 ap.add_argument('--summarise_family',required=False,default=False, action='store_true',help='Summarise CAZY gene models by CAZY functional family')
+ap.add_argument('--by_contig',required=False,default=False, action='store_true',help='Summarise CAZY gene models by contig rather than accross the whole genome')
+ap.add_argument('--trim_gene_id',required=False,default='0', type=int,help='trim the final xbp from each gene id, so that numbers reflect gene numbers rather than proteins')
+ap.add_argument('--kubicek_2014',required=False,default=False, action='store_true',help='Summarise CAZY families by functions described in Kubicek et al. 2014')
+
 # ap.add_argument('--out_secreted',required=True,type=str,help='headers of secreted CAZY genes')
 # ap.add_argument('--out_by_contig',required=True,type=str,help='breakdown of secreted CAZY gene families by contig')
 
@@ -40,6 +44,45 @@ with open(conf.inp_gff) as f:
     gff_lines = f.readlines()
 
 summarise_family = conf.summarise_family
+by_contig = conf.by_contig
+trim_id = conf.trim_gene_id
+kubicek_2014 = conf.kubicek_2014
+
+# kubicek_2014_dict =
+kubicek_2014_dict = defaultdict(str, {
+    "GH6.hmm" : "Cellulases",
+    "GH7.hmm" : "Cellulases",
+    "GH5.hmm" : "Cellulases",
+    "GH12.hmm" : "Cellulases",
+    "GH45.hmm" : "Cellulases",
+    "GH1.hmm" : "B-Glycosidases",
+    "GH3.hmm" : "B-Glycosidases",
+    "GH61.hmm" : "Accessory enzymes",
+    "CBM.hmm" : "Accessory enzymes",
+    "GH10.hmm" : "Xylanases",
+    "GH11.hmm" : "Xylanases",
+    "GH30.hmm" : "Xylanases",
+    "GH74.hmm" : "Xyloglucanases",
+    "GH27.hmm" : "A-Galactosidases",
+    "GH36.hmm" : "A-Galactosidases",
+    "GH26.hmm" : "B-Mannase",
+    "GH43.hmm" : "A-Arabinosidases",
+    "GH51.hmm" : "A-Arabinosidases",
+    "GH54.hmm" : "A-Arabinosidases",
+    "GH62.hmm" : "A-Arabinosidases",
+    "GH35.hmm" : "B-Galactosidases",
+    "GH67.hmm" : "B-Glucuronidases",
+    "GH115.hmm" : "B-Glucuronidases",
+    "GH28.hmm" : "Polygalacturonase",
+    "GH78.hmm" : "Polygalacturonase",
+    "PL1.hmm" : "Polygalacturonate lyases",
+    "PL3.hmm" : "Polygalacturonate lyases",
+    "PL4.hmm" : "Polygalacturonate lyases",
+    "PL9.hmm" : "Polygalacturonate lyases",
+    "PL11.hmm" : "Polygalacturonate lyases"
+    }
+)
+
 
 #-----------------------------------------------------
 # Step 2
@@ -51,6 +94,8 @@ for line in secreted_lines:
     line = line.rstrip()
     split_line = line.split()
     gene_id = split_line[0].replace(">", "")
+    if trim_id > 0:
+        gene_id = gene_id[:-int(trim_id)]
     secreted_set.add(gene_id)
 
 # print secreted_set
@@ -61,6 +106,8 @@ for line in secreted_lines:
 # Process CAZY file
 #-----------------------------------------------------
 
+# Hmm2function_dict = { }
+
 seen_set = Set([])
 family_set = Set([])
 gene_dict = defaultdict(list)
@@ -70,9 +117,17 @@ for line in inp_lines:
     split_line = line.split()
     family = split_line[0]
     if summarise_family == True:
-        family = family[0:2]
-        family = family.replace("CB", "CBM")
+        if kubicek_2014 == True:
+            # print family
+            if not kubicek_2014_dict[family]:
+                continue
+            family = kubicek_2014_dict[family]
+        else:
+            family = family[0:2]
+            family = family.replace("CB", "CBM")
     gene_id = split_line[2]
+    if trim_id > 0:
+        gene_id = gene_id[:-int(trim_id)]
     # print gene_id
     if gene_id in secreted_set and gene_id not in seen_set:
         seen_set.add(gene_id)
@@ -82,7 +137,11 @@ for line in inp_lines:
 
 for family in family_dict.keys():
     family_sz = len(family_dict[family])
-    # print " - ".join([str(family), str(family_sz)])
+    if by_contig == False:
+        print " - ".join([str(family), str(family_sz)])
+
+if by_contig == False:
+    quit()
 
 #-----------------------------------------------------
 # Step 4
@@ -100,6 +159,8 @@ for line in gff_lines:
         col9_split = col9.split(";")
         gene_id = col9_split[0]
         gene_id = gene_id.replace("ID=", "")
+        if trim_id > 0:
+            gene_id = gene_id[:-int(trim_id)]
         contig = split_line[0]
         gene_count_dict[contig] += 1
         if gene_id in seen_set:
